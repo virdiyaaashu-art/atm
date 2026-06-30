7ea5c7a2a4a9e716e487d4e24aa5e131f7d9018d
virdiyaaashu-art <virdiyaaashu@gmail.com>
2026-06-30 19:04:01 +0530

    Add files via upload

---
+++ markd2.py
@@ -0,0 +1,40 @@
+import streamlit as st
+st.markdown("# tattvam")
+st.markdown("## tattvam")
+st.markdown("### tattvam")
+st.markdown("#### tattvam")
+
+
+st.markdown("**bold text**")
+st.markdown("*italic text*")
+st.markdown("***italic and bold***")
+st.markdown("~~strictrough~~")
+
+
+st.markdown("""
+###fruits
+-apple
+-mango
+""")
+
+
+st.markdown("""
+###vegetables
+1.tomato
+2.bringele
+""")            
+            
+
+st.markdown("""
+### Student Table
+
+| Name | Age | City |
+|------|----:|------|
+| Parth | 22 | Ahmedabad |
+| Rahul | 25 | Surat |
+| Amit | 24 | Rajkot |
+""")
+
+
+st.markdown("###emojs")
+st.markdown("📚💻✍🏼📓⎛⎝ ≽ > ⩊ < ≼ ⎠⎞")
---
+++ timetable.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("""
+### timetable
+|subject|teacher|books|
+|-------|------:|----:|            
+|maths  |trupti |foundation mathamatices|
+|science|madhu  |conected to science|
+""")                                                       
