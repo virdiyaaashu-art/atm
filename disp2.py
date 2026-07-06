83ce8f5c9c246afdf376f5f8f1f900fe70ba3f62
virdiyaaashu-art <virdiyaaashu@gmail.com>
2026-07-06 18:40:03 +0530

    Add files via upload

---
+++ disp2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("Success Example")
+st.success("Login Successful!")
+st.info("Your profile is 80% complete.")
+st.warning("Password will expire in 3 days.")
+st.error("Invalid Username or Password!")
