4e9431adba1228aba8431dbbe6d1db9d738a4f65
virdiyaaashu-art <virdiyaaashu@gmail.com>
2026-07-08 18:58:08 +0530

    Add files via upload

---
+++ logo2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.logo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ-1LOml1tXe6H5_j6lULlylf8nQSQ4yjxd4ctsWDFdLEmRVse0o1-G3q8&s=10")
+st.sidebar.title("Menu")
+st.sidebar.button("Home")
+st.sidebar.button("About")
+st.title("Dashboard")
